1d62beea01025ddd17380ed8c0dc1838f83ce843
GumiGumi256 <egumisiriza5@gmail.com>
2024-07-16 08:59:04 +0300

    maintainance screen

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        // <Navbar />
-        // {children}
-        // <Footer />
+         {/* <Navbar />
+         {children}
+         <Footer /> */}
     <div className="flex justify-center items-center h-screen ">
     <p className="bold-20 md:bold-32">
     We are under maintainance, we will be back shortly
