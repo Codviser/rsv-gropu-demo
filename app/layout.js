48011a81bd2be8ebde455bec7ff23da14c7981bd
Codviser <145702684+Codviser@users.noreply.github.com>
2024-07-24 12:15:02 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,14 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-         {/* <Navbar />
+          <Navbar />
          {children}
-         <Footer /> */}
-    <div className="flex justify-center items-center h-screen ">
-    <p className="bold-20 md:bold-32">
-    We are under maintainance, we will be back shortly
-    </p>
-    </div>
+         <Footer /> 
+ 
       </body>
     </html>
   );
